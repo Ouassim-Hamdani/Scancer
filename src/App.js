4f77fd99c6b7f03455dd56c8bdce8c8e0f766568
ouassim-hamdani <ouassim5gamers@gmail.com>
2022-12-16 22:01:22 +0100

    Initial Push with navbar & SideBar & Grid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { NavBar } from './components/NavBar';
+import { SideBar } from './components/SideBar';
+import {Card} from "./components/Card"
+let name="Anis ki zabi"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-white h-screen">
+        <NavBar></NavBar>
+        <SideBar></SideBar>
+        <div className='grid grid-cols-4 grid-rows-2 h-2/3  gap-10 items-end ml-36 mt-28 w-5/6'>
+          <Card className="col-span-1"></Card>
+          <Card className="col-span-1"></Card>
+          <div className='shadow-xl col-span-2'>{name}</div>
+          
+        </div>
+        
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",".src/components/*"
+  ],
+  theme: {
+    extend: {
+      colors : {
+        primary:"#4264D0",
+        secondary : "#303778",
+        nav: "#F4F4F8",
+        element : "#EAEAEA",
+        itemGray : "#3A3A3A",
+        title : "#2E2E2E"
+      }
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+}
