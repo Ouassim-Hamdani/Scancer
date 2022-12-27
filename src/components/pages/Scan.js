473fc058907dd223e97127389c443fa6a63dd6c2
ouassim-hamdani <ouassim5gamers@gmail.com>
2022-12-27 19:47:55 +0100

    Implemented Result componenet

--- src/components/pages/Scan.js
+++ src/components/pages/Scan.js
@@ -3,7 +3,8 @@ import { SideBar } from '../SideBar';
 import {Upload} from "../scan_component/Upload"
 import { Uploading } from '../scan_component/Uploading';
 import { Process } from '../scan_component/Process';
-let states = {uploading:{container:"h-5/6", smallContainer:"h-2/3"},process:{container:"h-5/6", smallContainer:"h-5/6"}}
+import { Result } from '../scan_component/Result';
+let states = {uploading:{container:"h-5/6", smallContainer:"h-2/3"},processResult:{container:"h-5/6", smallContainer:"h-5/6"}}
 export const Scan = () => {
   
   return (
@@ -13,7 +14,7 @@ export const Scan = () => {
         <div className='space-y-4 pt-8 h-5/6'>
             <span className="text-gray-900 pl-36 text-3xl font-semibold">Scan</span>
             <div className='flex justify-center items-center h-5/6'>
-                <Process/>
+                <Result/>
             </div>
         </div>
         
