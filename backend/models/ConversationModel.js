697b4c23ba0c60337fbb41762c3ea4c6868d99e4
Sami-Krim <s_krim@estin.dz>
2023-01-09 13:04:16 +0100

    Redone
    Nbeddel structure

--- backend/models/ConversationModel.js
+++ backend/models/ConversationModel.js
@@ -3,38 +3,15 @@ const Schema = mongoose.Schema
 
 const ConversationSchema = new mongoose.Schema(
     {
-        members : [{
-            type : mongoose.Schema.Types.ObjectId,
-            refPath : 'members.model_type',
-            model_type : {
-                type : String,
-                enum : ['Patient', 'Doctor'],
-                required : true
-            }
-        }]
+        members : Array
     },
     {timestamps : true}
 );
 
 ConversationSchema.statics.newConversation = async function (sender, receiver) {
-    let send = {sender, model_type : 'Doctor'};
-    const doct = await Doctor.findOne({"_id": receiver})
-    if(doct){
-        let receiv = {receiver, model_type : 'Doctor'};
-        let info = [send, receiv];
-        const conversation = await this.create({members : info});
-        return conversation;
-    } else {
-        const pat = await Doctor.findOne({"_id": receiver});
-        if(pat){
-            let receiv = {receiver, model_type : 'Patient'};
-            let info = [send, receiv];
-            const conversation = await this.create({members : info});
-        return conversation;
-        } else {
-            throw Error('User does not exist!');
-        }
-    }
+    let info = [sender, receiver];
+    const conversation = await this.create({members : info});
+    return conversation;
 }
 
 module.exports = mongoose.model("Conversation", ConversationSchema);
